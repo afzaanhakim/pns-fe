1059dc5b702b8dac8d0564e163aa7bb9b363e76b
afzaanhakim <afzaanhakim19@gmail.com>
2022-02-22 09:56:59 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
 
@@ -15,20 +13,13 @@ const App = () => {
 				<div className="header-container">
 					<header>
             <div className="left">
-              <p className="title">🐱‍👤 Ninja Name Service</p>
+              <p className="title">⚡ Pika Name Service ⚡</p>
               <p className="subtitle">Your immortal API on the blockchain!</p>
             </div>
 					</header>
 				</div>
 
         <div className="footer-container">
-					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
 				</div>
 			</div>
 		</div>
